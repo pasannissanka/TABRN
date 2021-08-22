af10e3c339ce01906ff9a01398113608c1bc8aaf
pasannissanka <pasannissanka@hotmail.com>
2021-08-22 17:23:22 +0530

    view controller changes

--- packages/api/src/controllers/view.controller.ts
+++ packages/api/src/controllers/view.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express';
 import { AppError } from '../helpers/errors/app_error';
 import { successResponse } from '../helpers/responses/success_response';
-import { ViewModel } from '../modules/workspace-view/model/view.model';
-import { ViewReqData } from '../modules/workspace-view/types/view.type';
+import { CalenderViewModel } from '../modules/workspace-view/model/calenderView.model';
+import { ListViewModel } from '../modules/workspace-view/model/listView.model';
+import { CalenderReqData } from '../modules/workspace-view/types/calenderView.type';
+import { ListViewReqData } from '../modules/workspace-view/types/listView.type';
 import { WorkspaceModel } from '../modules/workspace/model/workspace.model';
 
 class ViewController {
-  public async createView(req: Request, res: Response) {
+  public async createViewListView(req: Request, res: Response) {
     const user = req.user;
-    const data = req.body as ViewReqData;
+    const data = req.body as ListViewReqData;
     const { workspaceId } = req.params;
 
     const workspaceData = await WorkspaceModel.findById(workspaceId);
@@ -16,13 +18,39 @@ class ViewController {
       throw new AppError('Invalid Workspace', 404);
     }
 
-    const view = new ViewModel({
+    const view = new ListViewModel({
       userId: user?.id,
       workspaceId: workspaceId,
-      type: data.type,
-      entryKind: data.entryKind,
       title: data.title,
       description: data.description,
+      filterProperties: data?.filterProperties,
+    });
+
+    await view.save();
+
+    successResponse(res, view);
+  }
+
+  public async createViewCalender(req: Request, res: Response) {
+    const user = req.user;
+    const data = req.body as CalenderReqData;
+    const { workspaceId } = req.params;
+
+    const workspaceData = await WorkspaceModel.findById(workspaceId);
+    if (!workspaceData) {
+      throw new AppError('Invalid Workspace', 404);
+    }
+
+    const view = new CalenderViewModel({
+      userId: user?.id,
+      workspaceId: workspaceId,
+      title: data.title,
+      description: data.description,
+      dateFormat: data.dateFormat,
+      timeFormat: data.timeFormat,
+      primaryTZ: data.primaryTZ,
+      secondaryTZ: data.secondaryTZ,
+      weekStart: data.weekStart,
     });
 
     await view.save();
