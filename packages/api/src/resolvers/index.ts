bd834ee416a5741b43bba21a9312f7113d8da4ce
pasannissanka <pasannissanka@hotmail.com>
2021-10-19 13:30:46 +0530

    deleted resolvers

--- packages/api/src/resolvers/index.ts
+++ packages/api/src/resolvers/index.ts
@@ -1,8 +1,6 @@
 import { schemaComposer } from 'graphql-compose';
-import ViewSchema from './view.resolver';
-import EntrySchema from './entry.resolver';
 
-schemaComposer.merge(ViewSchema);
-schemaComposer.merge(EntrySchema);
+// schemaComposer.merge(ViewSchema);
+// schemaComposer.merge(EntrySchema);
 
 export default schemaComposer.buildSchema();
